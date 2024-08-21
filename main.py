e8561b6333e6e5a6d63e59935564ca183d4f5696
ongoing211 <56183731+ongoing211@users.noreply.github.com>
2024-08-21 23:39:59 +0300

    Add files via upload
    Продолжение

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from random import *
+
+root = Tk()
+root.title('Камень Ножницы Бумага') #название
+root.geometry('600x400') #разрешение окна
+root.resizable(width=False, height=False) #запрет на изменение размера окна
+
+
+root.mainloop()
