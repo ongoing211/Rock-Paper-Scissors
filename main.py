7022efc1fea2581db49bf5e9a6b1c4ccb3b9628f
ongoing211 <56183731+ongoing211@users.noreply.github.com>
2024-08-22 19:01:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ root = Tk()
 root.title('Камень Ножницы Бумага') #название
 root.geometry('600x400') #разрешение окна
 root.resizable(width=False, height=False) #запрет на изменение размера окна
+root['bg'] = 'pink' #параметры bg и fg - цвета фона и текста
+
+labelText = Label(root, text='hi', fg='white', font=('Comic Sans MS', 20), bg='black')
+labelText.place(y=175, x=275)
 
 
 root.mainloop()
