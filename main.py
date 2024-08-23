4acfd1674260d0fa8f8cbe32e5fb37eaad306052
ongoing211 <56183731+ongoing211@users.noreply.github.com>
2024-08-23 23:44:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,32 @@ root.geometry('600x400') #разрешение окна
 root.resizable(width=False, height=False) #запрет на изменение размера окна
 root['bg'] = 'pink' #параметры bg и fg - цвета фона и текста
 
-labelText = Label(root, text='hi', fg='white', font=('Comic Sans MS', 20), bg='black')
-labelText.place(y=175, x=275)
 
+def knb():
+    knb = ['Камень', 'Ножницы', 'Бумага'] #список
+    value = choice(knb)
+    labelText.configure(text=value)
+
+
+labelText = Label(root, text='Раз, два, три!', fg='black', font=('Comic Sans MS', 20), bg='white')
+labelText.place(y=125, x=220)
+
+stone = Button(root,
+               text='Камень',
+               font=('Comic Sans MS', 20),
+               bg='white') #виджет кнопки
+stone.place(y=200, x=85)
+
+stone = Button(root,
+               text='Ножницы',
+               font=('Comic Sans MS', 20),
+               bg='white') #виджет кнопки
+stone.place(y=200, x=230)
+
+stone = Button(root,
+               text='Бумага',
+               font=('Comic Sans MS', 20),
+               bg='white') #виджет кнопки
+stone.place(y=200, x=405)
 
 root.mainloop()
